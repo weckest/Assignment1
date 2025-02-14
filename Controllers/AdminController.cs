41ea23e6f26938fe1dbf1f0eb30361b414108fcf
Logan Pedersen <lpedersen15@my.bcit.ca>
2025-02-14 13:02:10 -0800

    made editing roles work

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Identity;
+using NuGet.Protocol;
 namespace MyApp.Namespace
 {
     [Authorize(Roles = "admin")]
@@ -33,8 +34,31 @@ namespace MyApp.Namespace
 
         public ActionResult EditUser(string id)
         {
+            ViewData["id"] = id;
             ViewData["Roles"] = new SelectList(_roleManager.Roles.ToList(), "Id", "Name");
+            Console.WriteLine(ViewData["Roles"].ToJson() + "\nSomething-----------------");
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(string id, string role)
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine(id);
+            Console.WriteLine(role);
+            Console.WriteLine("-----------------");
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+            var roles = await _userManager.GetRolesAsync(user);
+            // try {
+                await _userManager.RemoveFromRolesAsync(user, roles.ToArray());
+                var roleName = _roleManager.Roles.FirstOrDefault(r => r.Id == role)?.Name;
+                await _userManager.AddToRoleAsync(user, roleName);
+            // } catch (Exception e) {
+                // Console.WriteLine(e.Message);
+            // }
+            
+            return RedirectToAction("Admin");
         }
 
     }
